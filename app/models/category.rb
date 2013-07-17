242b873d0cc5c22b3d8d452a2d076b911701ee90
Matthew Harwood <matthhar12@gmail.com>
2013-07-17 14:48:55 -0700

    added show interactions to the site

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,6 +3,6 @@ class Category < ActiveRecord::Base
   has_many	:categoryworks
   has_many :works, :through => :categoryworks
   accepts_nested_attributes_for :works
-  attr_accessible :description, :name, :category_ids, :svg
+  attr_accessible :description, :name, :category_ids, :svg, :post_a, :post_b, :post_c, :post_d
 
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -7,7 +7,7 @@ class Work < ActiveRecord::Base
   has_many	:categoryworks
   has_many :categories, :through => :categoryworks
   accepts_nested_attributes_for :categories
-  attr_accessible :name, :subtitle, :category_ids, :svg
+  attr_accessible :name, :subtitle, :category_ids, :svg, :post_a, :post_b, :post_c, :post_d
 
    def categoryworks=(works)
    	binding.pry
